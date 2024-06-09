5c681cfc916ef4d97ac9edf5d74eccea00515841
Shivaramsagar3054 <sagarshivaram44@gmail.com>
2024-06-09 21:30:17 +0530

    Update insertion sorting.cpp

--- insertion sorting.cpp
+++ insertion sorting.cpp
@@ -2,19 +2,17 @@
 int main(){
 	int arr[]={8,9,21,10,20,19,11,12,18,1,2,3,4};
 	int n=sizeof(arr)/sizeof(arr[0]);
-	int a,b;
 	for(int i=1;i<n;i++){
-		a=arr[i];
-		b=i-1;
-		while(arr[b]>a){
-			arr[b+1]=arr[b];
-			b=b-1;
+		int t=arr[i];
+		int j;
+		for(j=i-1;(j>=0 && t<arr[j]);j--){
+			arr[j+1]=arr[j];
 		}
-		arr[b+1]=a;
-		
+		arr[j+1]=t;
+	
 	}
 	for(int i=0;i<n;i++){
-	printf("%d",arr[i]);
+	printf("%d,",arr[i]);
 	}
 	return 0;
 }
